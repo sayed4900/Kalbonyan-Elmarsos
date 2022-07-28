dd042b3035ba304d99a2ace4b068511346d571a6
sayed4900 <103145317+sayed4900@users.noreply.github.com>
2022-07-28 05:44:31 +0200

    Update challenge4.js

--- 02-Udemy/-02-Js-Jonas/Challenges/Working With Arrays/04-Challenges/challenge4.js
+++ 02-Udemy/-02-Js-Jonas/Challenges/Working With Arrays/04-Challenges/challenge4.js
@@ -50,3 +50,54 @@ const dogs = [
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 //1
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+//2
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  }`
+);
+
+//3
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .map(dog => dog.owners)
+  .flat();
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+//4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much`);
+
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little`);
+
+//5
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+//6
+console.log(
+  dogs.some(
+    dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1
+  )
+);
+
+//7
+console.log(
+  dogs.filter(
+    dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1
+  )
+);
+
+//8
+
+const dogsCopy = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+
+console.log(dogsCopy);
