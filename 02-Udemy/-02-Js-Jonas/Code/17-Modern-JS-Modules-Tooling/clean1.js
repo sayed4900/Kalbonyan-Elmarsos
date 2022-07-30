e546e54b8bc04af31e81dbcc489d77809d5c049c
sayed4900 <103145317+sayed4900@users.noreply.github.com>
2022-07-30 14:54:32 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Code/17-Modern-JS-Modules-Tooling/clean1.js
@@ -0,0 +1,58 @@
+var budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+var spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+var addExpense = function (value, description, user) {
+  if (!user) user = 'jonas';
+  user = user.toLowerCase();
+
+  // var lim;
+  // if (limits[user]) {
+  //   lim = limits[user];
+  // } else {
+  //   lim = 0;
+  // }
+  const limit = getLimit(user);
+
+  if (value <= limit) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+console.log(budget);
+
+const checkExpenses = function () {
+  for (const entry of budget) {
+    // const limit = spendingLimits?.[entry] ?? 0;
+
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+  }
+};
+checkExpenses();
+
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const el of budget)
+    output += el.value <= -bigLimit ? `${el.description.slice(-2)} /` : '';
+
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+console.log(budget);
+logBigExpenses(100);
